ef933446e38d92018cb10f9b927efe7f2b947ed6
martins-a <at-martins@outlook.com>
2022-06-15 18:03:33 -0300

    Student configured with db

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,25 +1,25 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getStudents() {
-        return List.of(new Student(
-                1L,
-                "Maria",
-                21,
-                LocalDate.of(2000, Month.JANUARY, 5),
-                "mariam.jamal@gmail.com")
-        );
+        return studentService.getStudents();
     }
 }
